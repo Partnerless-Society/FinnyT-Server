7aa067838a100184d93298b4123f88ee58005077
Narihito <heinboss234@gmail.com>
2026-04-06 19:31:31 +0630

    .

--- src/services/aiservice.js
+++ src/services/aiservice.js
@@ -7,19 +7,19 @@ import { editSheetData, readSheetData } from "../tools/googlesheettool.js";
 
 //Analyse Ai
 export const Ai = new ChatGroq({
-   apiKey : process.env.Ai,
+   apiKey : process.env.AI,
    model : "moonshotai/kimi-k2-instruct",
 })
 
 //Vision Ai
 export const Aiimage = new ChatGroq({
-    apiKey : process.env.Ai,
+    apiKey : process.env.AI,
     model : "meta-llama/llama-4-scout-17b-16e-instruct"
 })
 
 //AiAgent to For Google Sheet Update, Auto Update and other services
 export const Aiagent = new ChatGroq({
-    apiKey : process.env.Ai,
+    apiKey : process.env.AI,
     model : "moonshotai/kimi-k2-instruct-0905"
 })
 
