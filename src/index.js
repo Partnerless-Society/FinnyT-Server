e1956186de0fa6d623271899e479aadc1e23bc9f
Narihito <heinboss234@gmail.com>
2026-04-06 20:14:20 +0630

    .

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://finnyt-project.vercel.app/",
     credentials: true
 }));
 app.use((err, req, res, next) => {
